81425173fcdd347618b5723a9e3fd432fcb9b4d6
peter-sigma <petergichehacrypto@gmail.com>
2024-01-16 10:48:17 +0000

    first task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,21 +2,37 @@
 """
 0-subs.py
 """
+
 import requests
+import sys
 
 def number_of_subscribers(subreddit):
-    """A function that Queries the Reddit API and returns the number of subscribers"""
+    """Queries the Reddit API and returns the number of subscribers"""
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'New-User-Agent'}
+    user_agent = {'User-agent': 'My-User-Agent'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
+    
     try:
+        response = requests.get(url, headers=user_agent)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        results = response.json()
         return results.get('data').get('subscribers')
-
-    except Exception:
+    except requests.RequestException as e:
+        print("Error making API request: {}".format(e))
+        print("Response content: {}".format(response.content))
         return 0
+    except Exception as e:
+        print("Error: {}".format(e))
+        return 0
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        subreddit_name = sys.argv[1]
+        subscribers_count = number_of_subscribers(subreddit_name)
+        print("The subreddit '{}' has {} subscribers.".format(subreddit_name, subscribers_count))
+    else:
+        print("Usage: python script_name.py subreddit_name")
+
