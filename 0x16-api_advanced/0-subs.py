b5aea3e3b4bbafa1a8befc053ac142164652a231
peter-sigma <petergichehacrypto@gmail.com>
2024-01-16 11:00:10 +0000

    first task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,38 +1,21 @@
 #!/usr/bin/python3
-"""
-0-subs.py
-"""
+"""A program to query subscribers on a Reddit subreddit."""
 
 import requests
-import sys
+
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers"""
+    """Return the total number of subscribers in a subreddit."""
 
-    if subreddit is None or not isinstance(subreddit, str):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by TechGuru)"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 404:
         return 0
 
-    user_agent = {'User-agent': 'My-User-Agent'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    
-    try:
-        response = requests.get(url, headers=user_agent)
-        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
-        results = response.json()
-        return results.get('data').get('subscribers')
-    except requests.RequestException as e:
-        print("Error making API request: {}".format(e))
-        print("Response content: {}".format(response.content))
-        return 0
-    except Exception as e:
-        print("Error: {}".format(e))
-        return 0
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        subreddit_name = sys.argv[1]
-        subscribers_count = number_of_subscribers(subreddit_name)
-        print("The subreddit '{}' has {} subscribers.".format(subreddit_name, subscribers_count))
-    else:
-        print("Usage: python script_name.py subreddit_name")
-
+    results = response.json().get("data")
+    return results.get("subscribers")
