ae37604c5a83aa173b4bdb6e601979244653904c
peter-sigma <petergichehacrypto@gmail.com>
2024-01-19 01:09:31 +0000

    top 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""A program to querry first 10 hot posts titles""""
-
-from requests import get
+'''
+   function top_ten
+'''
+import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts in a given subreddit
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    all_data = response.json()
-
+    '''
+        returns the top ten hot posts for a given subreddit
+    '''
+    user = {'User-Agent': 'peesigma'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        data = all_data.get('data').get('children')
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-        for i in data:
-            print(i.get('data').get('title'))
 
-    except:
-        print("None")
+if __name__ == "__main__":
+    top_ten(argv[1])
