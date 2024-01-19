93914161c12f2156379ab5ab91f8fc5bc0815ec4
peter-sigma <petergichehacrypto@gmail.com>
2024-01-19 00:45:36 +0000

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts in a given subreddit
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    all_data = response.json()
+
+    try:
+        data = all_data.get('data').get('children')
+
+        for i in data:
+            print(i.get('data').get('title'))
+
+    except:
+        print("None")
