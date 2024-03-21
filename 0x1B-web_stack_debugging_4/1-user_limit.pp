adb0a56c113dcae461b59e53f7e8671506151adc
peter-sigma <petergichehacrypto@gmail.com>
2024-03-21 06:05:03 +0000

    login user without error

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+#fix os configuration to login without errors
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
