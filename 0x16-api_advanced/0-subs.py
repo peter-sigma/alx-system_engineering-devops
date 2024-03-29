a168aab9a67e8044fd70abc7828191e3ce38babc
peter-sigma <petergichehacrypto@gmail.com>
2024-03-29 06:45:16 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,49 @@
 #!/usr/bin/python3
-"""Query subscribers in a given subreddit."""
-import requests
+"""Function to query subscribers on a given Reddit subreddit."""
+'''import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns total number of subscribers in a given subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")'''
+
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "sigma-peApp/1.0 (by sigma-pe)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    print("URL:", url)
+    print("Headers:", headers)
+
+    print("Response Headers:", response.status_code)    
+    # Check if the response is successful
+    if response.status_code != 200:
+        print("Status code not ok!!!")
+        return 0
+    
     try:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {
-            "User-Agent": "sigma:0x16.api.advanced"
-        }
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
+        data = response.json()
+        # Check if 'data' key exists in the JSON response
+        if 'data' in data:
+            subscribers = data['data'].get('subscribers', 0)
+            return subscribers
+        else:
+            print("ok but no data to retrieve" )
             return 0
-        results = response.json().get("data")
-        return results.get("subscribers")
-    except (Exception):
-        return (0)
+    except Exception as e:
+        print("An error occurred:", e)
+        return 0
+
+
